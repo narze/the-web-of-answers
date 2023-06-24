8408aeca9dff895e3cccb900fb25f44aca52b860
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-24 16:02:10 +0700

    Try panda CSS

---
+++ panda.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@pandacss/dev';
+
+export default defineConfig({
+	// Whether to use css reset
+	preflight: true,
+
+	// Where to look for your css declarations
+	include: ['./src/**/*.{js,ts,svelte}'],
+	// Files to exclude
+	exclude: [],
+
+	// Useful for theme customization
+	theme: {
+		extend: {},
+	},
+
+	// The output directory for your css system
+	outdir: 'styled-system',
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { css, cva } from 'styled-system/css';
+	import { container } from 'styled-system/patterns';
 	import entriesJson from '../entries.json';
 
 	const entriesCount = entriesJson.entries.length;
@@ -7,16 +9,34 @@
 		const id = 1 + Math.floor(Math.random() * entriesCount);
 		document.location.href = `/${id}`;
 	}
+
+	const button = cva({
+		base: {
+			display: 'flex',
+		},
+		variants: {
+			visual: {
+				solid: { bg: 'red.200', color: 'white' },
+				outline: { borderWidth: '1px', borderColor: 'red.200' },
+			},
+			size: {
+				sm: { padding: '4', fontSize: '12px' },
+				lg: { padding: '8', fontSize: '24px' },
+			},
+		},
+	});
 </script>
 
-<h1>เว็บนี้มีคำตอบ</h1>
+<main class={container({})}>
+	<h1 class={css({ fontSize: '3xl' })}>เว็บนี้มีคำตอบ</h1>
 
-<h2>วิธีใช้</h2>
+	<h2 class={css({ fontSize: 'xl' })}>วิธีใช้</h2>
 
-<ol>
-	<li>ตั้งคำถามแบบปลายปิดในใจ เช่น วันหยุดนี้ไปเที่ยวดีไหม หรือ เย็นนี้กินกะเพราดีหรือเปล่า</li>
-	<li>รวบรวมสมาธิกับคำถาม 10-15 วินาที</li>
-	<li>คลิกที่ปุ่มเพื่อรับคำตอบ</li>
-</ol>
+	<ol>
+		<li>ตั้งคำถามแบบปลายปิดในใจ เช่น วันหยุดนี้ไปเที่ยวดีไหม หรือ เย็นนี้กินกะเพราดีหรือเปล่า</li>
+		<li>รวบรวมสมาธิกับคำถาม 10-15 วินาที</li>
+		<li>คลิกที่ปุ่มเพื่อรับคำตอบ</li>
+	</ol>
 
-<button on:click={handleClick}>รับคำตอบ</button>
+	<button on:click={handleClick} class={button({ visual: 'solid', size: 'lg' })}>รับคำตอบ</button>
+</main>
