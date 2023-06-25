76e7e2c904e8e1f65bd1e09160ca03d98f463441
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-25 15:39:20 +0700

    Support cors

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -8,5 +8,14 @@ export const GET = (() => {
 
 	const entry = entriesFile.entries[idx];
 
-	return json({ result: entry });
+	return json(
+		{ result: entry },
+		{
+			headers: {
+				'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': '*',
+			},
+		},
+	);
 }) satisfies RequestHandler;
